8c2a31dbceb49cf05f97a569931cc0b27ed6475d
KernAttila <5556461+KernAttila@users.noreply.github.com>
2026-04-13 02:16:25 +0200

    fix: needed to go deeper in the datastructure with the "options" key under "multi_select" items.

--- notion_parser.py
+++ notion_parser.py
@@ -57,13 +57,9 @@ def parse_select(item_select):
 def parse_multi_select(item_multi_select):
     if item_multi_select is None:
         return None
-    return [select["name"] for select in item_multi_select["multi_select"]]
+    return [select["name"] for select in item_multi_select["multi_select"]["options"]]
 
 def parse_place(item_place):
     if item_place is None:
         return None
     return item_place
-    # address = item_place["address"]
-    # latitude = item_place["lat"]
-    # longitude = item_place["lon"]
-    # return f"{address} (lat: {latitude}, lon: {longitude})"
